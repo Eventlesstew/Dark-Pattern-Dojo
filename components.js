4e0fff5658573c092271acc6cfe33c8f73b3f6f5
Eventlesstew <161836208+Eventlesstew@users.noreply.github.com>
2025-06-07 17:38:34 +1000

    attempt

---
+++ components.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', ()=>{ /*How to trigger code.*/
+    addHeader();
+})
+
+async function addHeader() {
+    let parser = new DOMParser();
+
+    const response = await fetch("/Dark-Pattern-Dojo/components.html");
+    let file = await response.text();
+    let item = parser.parseFromString(file, "text/html");
+
+    let contentDiv = document.createElement("div");
+
+    let bodyDiv = document.createElement("div");
+    contentDiv.appendChild(item.getElementsByTagName("left")[0]);
+
+    bodyDiv.appendChild(document.body.innerHTML);
+    contentDiv.appendChild(bodyDiv);
+
+    document.body.appendChild(contentDiv);
+
+    let itemInternal = item.getElementsByTagName("internal")[0];
+    document.body.appendChild(itemInternal);
+
+
+}
