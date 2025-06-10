acba5fc67e37de9834f031e354a4c36b5d2c2db3
Eventlesstew <161836208+Eventlesstew@users.noreply.github.com>
2025-06-10 11:02:45 +1000

    update components

--- components.js
+++ components.js
@@ -9,20 +9,8 @@ async function addHeader() {
     let file = await response.text();
     let item = parser.parseFromString(file, "text/html");
 
-    let contentDiv = document.createElement("div");
-
-    let leftSidebar = item.getElementsByClassName("left")[0]
-    contentDiv.appendChild(leftSidebar);
-
-    let bodyDiv = document.createElement("div");
+    let bodyDiv = item.getElementsByClassName("main")[0];
     bodyDiv.innerHTML = document.body.innerHTML;
-    contentDiv.appendChild(bodyDiv);
-
     document.body.innerHTML = ""
-    document.body.appendChild(contentDiv);
-
-    let itemInternal = item.getElementsByClassName("internal")[0];
-    document.body.appendChild(itemInternal);
-
-
+    document.body.appendChild(item);
 }
