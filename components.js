76852d49ac039f312889c49d3739cff448c39867
Eventlesstew <becbenzac@gmail.com>
2025-06-14 10:07:34 +1000

    Rework components to fix dropdown

--- components.js
+++ components.js
@@ -2,6 +2,18 @@ document.addEventListener('DOMContentLoaded', ()=>{ /*How to trigger code.*/
     addHeader();
 })
 
+function dropdown(id) {
+    const leftDiv = item.getElementById("compLeft")
+    const dropdownElement = leftDiv.getElementsByClassName("dropdown")[id];
+    let dropdownContent = dropdownElement.getElementsByClassName("dropdown-content")[0];
+
+    if (dropdownContent.style.display == "none") {
+        dropdownContent.style.display = "block";
+    } else {
+        dropdownContent.style.display = "none";
+    }
+}
+
 async function addHeader() {
     let parser = new DOMParser();
 
@@ -9,7 +21,13 @@ async function addHeader() {
     let file = await response.text();
     let item = parser.parseFromString(file, "text/html");
 
-    let mainDiv = item.getElementsByClassName("middle")[0];
+    const leftDiv = item.getElementById("compLeft")
+    let dropdownButtons = leftDiv.getElementsByClassName("dropdown-button");
+    for (i = 0; i < dropdownButtons.length; i++) {
+        dropdownButtons[i].addEventListener("click", dropdown(i));
+    }
+
+    const mainDiv = item.getElementById("compMiddle");
     mainDiv.innerHTML = document.body.innerHTML;
     document.body.innerHTML = "";
     document.body.appendChild(item.getElementsByClassName("body")[0]);
