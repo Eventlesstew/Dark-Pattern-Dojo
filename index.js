ee2e543221c38c6a8553facbdef1b588f7b18a82
Eventlesstew <161836208+Eventlesstew@users.noreply.github.com>
2025-06-24 12:05:01 +1000

    Update index.js

--- index.js
+++ index.js
@@ -9,13 +9,11 @@ async function addHeader() {
     const response = await fetch("/Dark-Pattern-Dojo/components.html");
     let file = await response.text();
     let item = parser.parseFromString(file, "text/html");
-
-    const leftDiv = item.getElementById("compLeft");
     const categories = ["Temporal", "Psychological", "Monetary"]
 
     for (i = 0; i < categories.length; i++) {
         let tableSector = document.getElementById("homeTable" + categories[i]);
-        let sidebarSector = leftDiv.getElementById("sidebar" + categories[i]).getElementsByClassName("dropdown-content")[0];
+        let sidebarSector = item.getElementById("sidebar" + categories[i]).getElementsByClassName("dropdown-content")[0];
         tableSector.appendChild(sidebarSector);
     }
 }
